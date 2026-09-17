de5a07a6abd2dd3ace6f738b2846872149530317
pranavbigburry1 <pranav@bigburry.com>
2026-09-17 23:11:19 +0530

    Use environment variable for API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,22 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 // Wallet
 export const getWallet = () => API.get("/wallet");
 
-export const depositMoney = (amount) =>
-  API.post("/wallet/deposit", { amount });
+export const depositMoney = (amount) => API.post("/wallet/deposit", { amount });
 
 export const withdrawMoney = (amount) =>
   API.post("/wallet/withdraw", { amount });
 
 // Transactions
-export const getTransactions = () =>
-  API.get("/transactions");
+export const getTransactions = () => API.get("/transactions");
 
 // Banks
-export const getBanks = () =>
-  API.get("/banks");
+export const getBanks = () => API.get("/banks");
+export default API;
