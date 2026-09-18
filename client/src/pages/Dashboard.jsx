7a553e6e05587e22e0eedc1a29c4a8a07f5632d2
pranavbigburry1 <pranav@bigburry.com>
2026-09-18 10:47:27 +0530

    Fix Chart import case

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,11 @@
 import BalanceCard from "../components/BalanceCard";
 import Transactions from "../components/Transactions";
-import Chart from "../components/Chart";
+import Chart from "../components/chart";
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white p-6">
-
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        
         <div className="md:col-span-2 space-y-6">
           <BalanceCard />
           <Chart />
@@ -16,9 +14,7 @@ const Dashboard = () => {
         <div>
           <Transactions />
         </div>
-
       </div>
-
     </div>
   );
 };
